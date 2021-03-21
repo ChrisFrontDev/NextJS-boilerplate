bdd297c59772f3ae36417d7fffd19d092187f36c
Christian Santos <christian.pinho@outlook.com>
2021-03-21 18:41:22 -0300

    21. Criando estilos no primeiro componente.

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,17 @@
+import * as S from './styles';
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>TypeScript.ReactJs,NextJs e Styled Components</S.Description>
+    <S.Illustrator
+      src="/img/hero-illustration.svg"
+      alt="Um desenvovedor em frente ao computador escrevendo o código"
+    />
+  </S.Wrapper>
 );
 export default Main;
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -10,5 +10,10 @@ describe('test <Main /> component', () => {
     ).toBeInTheDocument();
   });
 
+  it('should render colors correctly', () => {
+    const { container } = render(<Main />);
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
+  });
+
   expect(container.firstChild).toMatchSnapshot();
 });
